dc419e170bbe6bb1c412f039ab39b7d94973b20d
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-10 12:36:48 -0300

    feat: Context Usuário

--- src/common/context/Usuario.js
+++ src/common/context/Usuario.js
@@ -1,15 +1,10 @@
 import { createContext, useState } from "react";
 
-
-
-
-
 export const UsuarioContext = createContext();
 
-
+UsuarioContext.displayName = "Usuário";
 
 export const UsuarioProvider = ({ children }) => {
-    
   const [nome, setNome] = useState("");
   const [saldo, setSaldo] = useState(0);
   return (
