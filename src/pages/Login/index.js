689169f02ac36eea1b42cce5fe164dc08ba7ca13
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-10 11:59:11 -0300

    feat: consumindo  ContextAPI com  o useContext

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,47 +3,41 @@ import { Container, Titulo, InputContainer } from "./styles";
 import { Input, InputLabel, InputAdornment } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import { UsuarioContext } from "common/context/Usuario";
+import { useContext } from "react";
 
 function Login() {
   const navigate = useNavigate();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
 
   return (
     <Container>
-      <UsuarioContext.Consumer>
-        {({nome, setNome, saldo, setSaldo}) => (
-          <>
-            <Titulo>Insira o seu nome</Titulo>
-            <InputContainer>
-              <InputLabel>Nome</InputLabel>
-              <Input
-                value={nome}
-                onChange={(event) => setNome(event.target.value)}
-                type="text"
-              />
-            </InputContainer>
-            <InputContainer>
-              <InputLabel>Saldo</InputLabel>
-              <Input
-                type="number"
-                value={saldo}
-                onChange={(event) => setSaldo(event.target.value)}
-                startAdornment={
-                  <InputAdornment position="start">R$</InputAdornment>
-                }
-              />
-            </InputContainer>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => {
-                navigate("/feira");
-              }}
-            >
-              Avançar
-            </Button>
-          </>
-        )}
-      </UsuarioContext.Consumer>
+      <Titulo>Insira o seu nome</Titulo>
+      <InputContainer>
+        <InputLabel>Nome</InputLabel>
+        <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>Saldo</InputLabel>
+        <Input
+          type="number"
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+        />
+      </InputContainer>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          navigate("/feira");
+        }}
+      >
+        Avançar
+      </Button>
     </Container>
   );
 }
