88327a27ca6cd57391ab5661972d11ae043bc55d
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-10 12:14:41 -0300

    feat: responsabilidade unica no router

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,23 @@
-import { UsuarioContext } from "common/context/Usuario";
+import { UsuarioProvider } from "common/context/Usuario";
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 function Rotas() {
-  const [nome, setNome] = useState("");
-  const [saldo, setSaldo] = useState(0);
-
   return (
     <Routes>
       <Route
         path="/"
         element={
-          <UsuarioContext.Provider
-            value={{
-              nome,
-              setNome,
-              saldo,
-              setSaldo,
-            }}
-          >
+          <UsuarioProvider>
             <Login />
-          </UsuarioContext.Provider>
+          </UsuarioProvider>
         }
       />
 
       <Route path="/feira" element={<Feira />} />
+
       <Route path="/carrinho" element={<Carrinho />} />
     </Routes>
   );
